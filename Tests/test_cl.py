c843103aaaabdf8c46082ddaf9d7a6d02e286a6a
Nora Wootten <norawootten@Noras-Laptop.local>
2025-01-27 13:31:37 -0600

    finalized tests file

---
+++ Tests/test_cl.py
@@ -0,0 +1,91 @@
+import unittest
+import subprocess
+from command_line import *
+from ProductionCode.get_food_data import*
+
+class Test_command_line(unittest.TestCase):
+    def test_main_command_line_list(self):
+        """tests the command_line file --list function to determine if it is functional"""
+        cmd = ["python3", "-u", "command_line.py", "--list", "CHEESE"]
+        # format of the line below retrieved from linked python subprocess article in assignment
+        code = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, text=True)
+        output, err = code.communicate()
+        self.assertEqual(output.strip(), 'CHEESE,BLUE\nCHEESE,BRICK\nCHEESE,BRIE')
+        code.terminate()
+
+    def test_main_command_line_list_edgeCase(self):
+        """tests the command_line file --list function to determine if it is functional"""
+        cmd = ["python3", "-u", "command_line.py", "--list", "APPLE"]
+        # format of the line below retrieved from linked python subprocess article in assignment
+        code = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, text=True)
+        output, err = code.communicate()
+        self.assertEqual(output.strip(), '')
+        code.terminate()
+    
+    def test_main_command_line_health_facts(self):
+        """tests the command_line file --healthfacts function to determine if it is functional"""
+        cmd = ["python3", "-u", "command_line.py", "--healthfacts", "CHEESE,BLUE"]
+        # format of the line below retrieved from linked python subprocess article in assignment
+        code = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, text=True)
+        output, err = code.communicate()
+        self.assertEqual(output.strip(), 'Alpha Carotene: 0.0\nAsh: 5.11\nBeta Carotene: 74.0\nBeta Cryptoxanthin: 0.0\nCarbohydrate: 2.34')
+        code.terminate()
+    
+    def test_main_command_line_health_facts_edgeCase(self):
+        """tests the command_line file --healthfacts function to determine if it is functional"""
+        cmd = ["python3", "-u", "command_line.py", "--healthfacts", "APPLE,HONEYCRISP"]
+        # format of the line below retrieved from linked python subprocess article in assignment
+        code = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, text=True)
+        output, err = code.communicate()
+        self.assertEqual(output.strip(), '')
+        code.terminate()
+
+
+class Test_get_food_data(unittest.TestCase):
+    def test_get_df(self):
+        """tests the get_df function in the get_food_data file to determine that it is creating the correct DataFrame"""
+        self.assertEqual("\n".join(list(get_df())),"Category\nDescription\nNutrient Data Bank Number\nData.Alpha Carotene\nData.Ash\nData.Beta Carotene\nData.Beta Cryptoxanthin\nData.Carbohydrate")
+    
+    def test__typeError_get_df(self):
+        """tests an edge case of the get_df function in the get_food_data file to determine that
+        the correct number of arguments were declared"""
+        with self.assertRaises(TypeError):
+            get_df("argument")
+
+    def test_fetch_category(self):
+        """tests the fetch_category function in the get_food_data file with a CHEESE example
+        to determine that it is functional"""
+        self.assertEqual("\n".join(list(fetch_category("CHEESE"))), 'CHEESE,BLUE\nCHEESE,BRICK\nCHEESE,BRIE')
+
+    def test_typeError_fetch_category(self):
+        """tests an edge case of the fetch_category function in the get_food_data file to determine that
+        enough arguments were declared"""
+        with self.assertRaises(TypeError):
+            fetch_category()
+    
+    def test_typeError2_fetch_category(self):
+        """tests an edge case of the fetch_category function in the get_food_data file to determine that
+        too many arguments were not declared"""
+        with self.assertRaises(TypeError):
+            fetch_category("argument 1", "argument 2")
+    
+    def test_health_facts(self):
+        """tests the health_facts function in the get_food_data file with a CHEESE,BLUE example
+        to determine that it is functional"""
+        self.assertEqual("\n".join(list(health_facts("CHEESE,BLUE"))), 'Data.Alpha Carotene\nData.Ash\nData.Beta Carotene\nData.Beta Cryptoxanthin\nData.Carbohydrate')
+
+    def test_typeError_health_facts(self):
+        """tests an edge case of the health_facts function in the get_food_data file to determine that
+        enough arguments were declared"""
+        with self.assertRaises(TypeError):
+            health_facts()
+    
+    def test_typeError2_health_facts(self):
+        """tests an edge case of the health_facts function in the get_food_data file to determine that
+        too many arguments were not declared"""
+        with self.assertRaises(TypeError):
+            health_facts("argument 1", "argument 2")
+
+
+if __name__ == '__main__':
+    unittest.main()
