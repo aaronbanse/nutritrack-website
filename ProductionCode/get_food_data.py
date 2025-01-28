6c90d0f8f0eaa9f4c7f5bfb219b83aecaa0968a0
Nora Wootten <norawootten@Noras-Laptop.local>
2025-01-27 19:41:47 -0600

    added some docstrings

--- ProductionCode/get_food_data.py
+++ ProductionCode/get_food_data.py
@@ -1,13 +1,16 @@
 import pandas as pd
 
 def get_df():
+    """returns csv file in the form of a dataframe"""
     df = pd.read_csv("Data/food_nutrition_sample.csv")
     return df
 
 def fetch_category(category):
+    """fetches appropriate columns via categories as strings"""
     df = get_df()
     return df[df["Category"]==category]["Description"]
 
 def health_facts(description):
+    """fetches appropriate columns via categories as strings"""
     df = get_df()
     return df[df["Description"] == description].drop(columns=["Category","Description","Nutrient Data Bank Number"])
