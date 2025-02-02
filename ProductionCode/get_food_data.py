db8a6c856638957498484b0e6f91979cdfe082f9
aaronbanse <acbanse@icloud.com>
2025-02-02 16:53:16 -0600

    Added typeError catches and renamed get_df to get_data for clarity, and updated function name in all instances. Also added dummy parameter to get_data which defaults to false, determines whether to pull from sample data set or not.

--- ProductionCode/get_food_data.py
+++ ProductionCode/get_food_data.py
@@ -1,16 +1,28 @@
 import pandas as pd
 
-def get_df():
-    """returns csv file in the form of a dataframe"""
-    df = pd.read_csv("Data/food_nutrition_sample.csv")
-    return df
+"""returns formatted error message"""
+def error(message):
+    return f"Error: {message}"
 
-def fetch_category(category):
-    """fetches appropriate columns via categories as strings"""
-    df = get_df()
-    return df[df["Category"]==category]["Description"]
+"""returns csv file in the form of a dataframe"""
+def get_data(dummy: bool = False):
+    if type(dummy) != bool:
+        raise(TypeError)
+    # set dataset name based on if dummy data requested
+    dataset_name = f"food_nutrition{'_sample' if dummy else ''}.csv"
+    data = pd.read_csv(f"Data/{dataset_name}")
+    return data
 
-def health_facts(description):
-    """fetches appropriate columns via categories as strings"""
-    df = get_df()
-    return df[df["Description"] == description].drop(columns=["Category","Description","Nutrient Data Bank Number"])
+"""fetches appropriate columns via categories as strings"""
+def fetch_category(category: str):
+    if type(category) != str:
+        raise(TypeError)
+    data = get_data(dummy=True)
+    return data[data["Category"]==category]["Description"]
+
+"""fetches appropriate columns via categories as strings"""
+def health_facts(description: str):
+    if type(description) != str:
+        raise(TypeError)
+    data = get_data(dummy=True)
+    return data[data["Description"] == description].drop(columns=["Category","Description","Nutrient Data Bank Number"])
