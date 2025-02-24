3c2b198403b3f7bda479d00a7abebc3796b4842a
Aaron Banse <bansea@carleton.edu>
2025-02-24 13:48:37 -0500

    Added link functionality to make user experience better

--- flask_app.py
+++ flask_app.py
@@ -9,50 +9,37 @@ app = Flask(__name__)
 # I am not familiar with HTML, so this is based on a few minutes of internet surfing... I assume we learn a bit more later in the course :)
 @app.route("/")
 def homepage():
-    return """
-    <h1>Welcome to The Homepage</h1>
-    <h3>Use this app to find useful information about foods, nutrition, and more</h3>
-    <p>
-    <h2>App Features:</h2>
-    <p>
-    <h3>1. Access information of a specific cell by appending the row and column of the cell you seek at the end of the browser URL above.</h3>
-    <h4>  ex. http://127.0.0.1:5000/Description/2 finds row 2 and column "Description".</h4>
-    <p>
-    <h3>2. Access a list of the related food types in the data set by typing /list/keyword at the end of the browser URL above.</h3>
-    <h4>  ex. http://127.0.0.1:5000/list/cheese to find the types of cheese in the data set.</h4>
-    <h4>  note: if the webpage returns blank, the food is not included in the data set.</h4>
-    <p>
-    <h3>3. Access a list of the nutritional information associated with a specific food by entering /health-facts/the specific food at the end of the browser URL above.</h3>
-    <h4>  ex. http://127.0.0.1:5000/health-facts/cheese,blue to find the nutritional data for blue cheese in the data set.</h4>
-    <h4>  note: if the webpage returns blank, the type of food is not included in the data set.</h4>
-    <br>
-    <h3>If you need to return to the homepage, enter http://127.0.0.1:5000 into your browser, though URLs may differ from machine to machine.</h3>
-    <h3>Take a look at (and maybe copy) your URL now so that you can come back to the homepage if you need to!</h3>
-    """
+    return render_template("index.html")
 
-"""
-@app.route("/<column_name>/<row>", strict_slashes = False)
-def get_cell(column_name: str, row: str):
-    # Accesses the information of a specific cell from an input column name and row.
-    
-    # Arguments:
-    # column_name : input from route
-    # row : input from route
-    
-    # column_name is not case-sensitive
-    
-    # column_name = column_name.title()
-    
-    # error checkss
-    if not row.isdigit():
-        return error("row must be an integer.")
-    if column_name not in dummy_data.columns:
-        return error(f"column name \"{column_name}\" not found")
-    if int(row) < 0 or int(row) >= len(dummy_data[column_name]) - 1:
-        return error(f"row index out of bounds. Only use indices from 0 to {len(dummy_data[column_name])-1}.")
-    
-    return str(dummy_data[column_name][int(row)])
-"""
+@app.route("/list/", strict_slashes = False)
+def get_foods_instructions():
+    return """
+            <html>
+            <h3><a href="/">Home</a></h3>
+            <body>
+            <h2>Find specific foods in the database</h2>
+            <p>Enter the food you would like to search up after "/list/".
+            <br>
+            You can click on each food item to see their nutrition facts.<p>
+            </body>
+            </html>
+            """
+
+@app.route("/health-facts/", strict_slashes = False)
+def get_nutrition_instructions():
+    return """
+            <html>
+            <h3><a href="/">Home</a></h3>
+            <body>
+            <h2>Find nutrition facts for specific foods</h2>
+            <p>
+            Enter the specific food item you would like to search up after "/healh-facts/". 
+            <br>
+            If the item was not found, make sure it exists using the "/list/" command.
+            <p>
+            </body>
+            </html>
+            """
 
 @app.route("/list/<category>", strict_slashes = False)
 def get_foods(category):
@@ -68,9 +55,10 @@ def get_foods(category):
     
     category = category.lower() # format for printing
     if len(items) > 0:
-        return ("The types of " + category + " in this data set are:<br>" + "<br>".join([item.title() for item in items]))
+        item_strs = [f"<a href=\"/health-facts/{item}\">{item.title()}</a>" for item in items]
+        return ("<h3><a href=\"/\">Home</a></h3><p>The types of " + category + " in this data set are:<br>" + "<br>".join(item_strs) + "</p>")
     else:
-        return ("The category '" + category + "' is not in the data set.")
+        return ("<h3><a href=\"/\">Home</a></h3><p>The category '" + category + "' is not in the data set.</p>")
 
 @app.route("/health-facts/<description>", strict_slashes = False)
 def get_nutrition(description):
@@ -87,9 +75,9 @@ def get_nutrition(description):
     if len(data) > 0:
         for i in range(len(labels)):
             food_info.append(str(labels[i] + ": " + str(data[i])))
-        return ("The nutrients included in '" + description.title() + "' are:<br>" + "<br>".join(food_info))
+        return ("<h3><a href=\"/\">Home</a></h3><p>The nutrients included in '" + description.title() + "' are:<br>" + "<br>".join(food_info) + "</p>")
     else:
-        return ("The food named '" + description.title() + "' is not in the data set. Would you like to search again?")
+        return ("<h3><a href=\"/\">Home</a></h3> <p>The food named '" + description.title() + "' is not in the data set. Would you like to search again?</p>")
 
 @app.errorhandler(404)
 def page_not_found(e):
